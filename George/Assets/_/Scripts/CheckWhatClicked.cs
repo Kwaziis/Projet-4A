86191fb973da39e591f80ab64d2870ca18913014
Kwaziis <glparthur03@gmail.com>
2022-12-17 18:19:25 +0100

    script overline+isdestroyed variable

--- George/Assets/_/Scripts/CheckWhatClicked.cs
+++ George/Assets/_/Scripts/CheckWhatClicked.cs
@@ -1,23 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class CheckWhatClicked : MonoBehaviour
 {
     // Start is called before the first frame update
+    private MonoBehaviour outlineScript, destruction;
+    public GameObject player;
+    bool CanBeDestroyed, IsDestroyed;
     void Start()
     {
         
+        outlineScript = GetComponent<Outline>();
+        //destruction = GetComponent<Destruction>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    if (!IsDestroyed){
+        float dist = Vector2.Distance(player.transform.position, transform.position);    
+        if (dist<=7){
+            CanBeDestroyed = true;
+            outlineScript.enabled = true; 
+        }
+        else{
+            CanBeDestroyed = false;
+            outlineScript.enabled = false; 
+        }
     }
-    
+    }
+
     void OnMouseDown()
     {
-        Debug.Log("poulet");
+        if (CanBeDestroyed)
+        {
+            //destruction.Invoke("Break", 0f);
+            outlineScript.enabled = false;
+            IsDestroyed = true;
+        }
     }
 }
