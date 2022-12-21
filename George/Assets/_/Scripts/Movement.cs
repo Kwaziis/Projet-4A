1316537029e330f5025d414553b90d5fd956a5e7
Dino <72475325+dinoclier72@users.noreply.github.com>
2022-12-21 17:01:05 +0100

    george aniamtions

--- George/Assets/_/Scripts/Movement.cs
+++ George/Assets/_/Scripts/Movement.cs
@@ -7,20 +7,28 @@ using UnityEngine.AI;
 public class Movement : MonoBehaviour
 {
     public Camera camera;
+    private Animator animation;
 
     private RaycastHit hit;
-
     private NavMeshAgent agent;
+
+    private Vector3 pos;
 
     private string groundTag ="Ground";
     // Start is called before the first frame update
     void Start()
     {
+        animation = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
+        pos = transform.position;
     }
     // Update is called once per frame
     void Update()
     {
+        if (pos != transform.position) 
+        {
+            animation.Play("walking", 0, -1);
+        }
         if (Input.GetMouseButtonDown(0))
         {
             Ray ray =camera.ScreenPointToRay(Input.mousePosition);
